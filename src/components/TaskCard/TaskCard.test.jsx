1df38ddb0d1d4054cd38cd787d96619e5ba1c1aa
Edlyn Evangelista <107823538+edlyn-e@users.noreply.github.com>
2022-09-09 13:44:15 +0930

    adds some test cases to consider

---
+++ src/components/TaskCard/TaskCard.test.jsx
@@ -0,0 +1,8 @@
+/*
+    test cases to consider: 
+    - should display task name
+    - should display button that represent if task is completed
+    - should change background colour depending on level of priority
+    - should have button to remove itself 
+
+*/
